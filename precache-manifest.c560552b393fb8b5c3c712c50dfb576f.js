16b7c753f934cb23c17cfdf6fc4aff2e866060d3
kiminno <nancyngo141@gmail.com>
2019-08-15 02:09:56 -0400

    Updates

---
+++ precache-manifest.c560552b393fb8b5c3c712c50dfb576f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb04a97512552b06acfff506fa103a5a",
+    "url": "/react-tic-tac-toe/index.html"
+  },
+  {
+    "revision": "21e9224c54365f02a83a",
+    "url": "/react-tic-tac-toe/static/css/main.832efcfc.chunk.css"
+  },
+  {
+    "revision": "3fec8b09d3e3e2136c47",
+    "url": "/react-tic-tac-toe/static/js/2.1f15b8d5.chunk.js"
+  },
+  {
+    "revision": "21e9224c54365f02a83a",
+    "url": "/react-tic-tac-toe/static/js/main.037c4746.chunk.js"
+  },
+  {
+    "revision": "084395ae5a1556397b3d",
+    "url": "/react-tic-tac-toe/static/js/runtime~main.1634a412.js"
+  }
+]);
